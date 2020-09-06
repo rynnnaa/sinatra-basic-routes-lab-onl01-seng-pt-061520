2b7c56f750ccce018871734ac461bf6c9f452d1e
Ryna Truong <rynatruong@outlook.com>
2020-09-06 22:44:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is __"
+  end 
   
-  get '/name' do
-    puts "My name is __"
-  end
-
+  get '/hometown' do 
+    "My hometown is ___"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is __"
+  end 
 end
